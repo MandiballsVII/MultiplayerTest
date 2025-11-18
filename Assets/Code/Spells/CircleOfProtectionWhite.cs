3e522db4e595f932229fdfe1aad8fd137b2548af
MandiballsVII <149199591+MandiballsVII@users.noreply.github.com>
2025-11-18 15:46:04 +0100

    Circulo de protección blanco
    - Crear un sistema de estados escalabe

---
+++ Assets/Code/Spells/CircleOfProtectionWhite.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleOfProtectionWhite : MonoBehaviour
+{
+    SpellData spellData;
+    void Start()
+    {
+        spellData = GetComponent<SpellData>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
